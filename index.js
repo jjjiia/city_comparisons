a66bc88dc1cea151f2020fb8b501e1f12aa4b09d
Jia <zhangjia@mit.edu>
2015-02-16 18:06:08 -0500

    cross city clicking ok need to make faster need to add filter draggin

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ var config = {
 
 
 var global = {
-	data: null,
-	nycPaths: null,
-	usMapWidth:600,
-	usMapHeight:600
+	nyc: null,
+	nycdata: null,
+	boston:null,
+	bostondata:null,
+	sf:null,
+	sfdata:null,
+	usMapWidth:400,
+	usMapHeight:400
 	
 }
 $(function() {
@@ -35,16 +39,53 @@ $(function() {
 })
 
 function dataDidLoad(error, nyc, nycdata, boston, bostondata, sf, sfdata) {
-	//var nycMap = initNycMap(ny, data, column, svg, max)
-	initNycMap(nyc, nycdata, "Income", "#svg-nyc", 250000)
-	initNycMap(boston, bostondata, "Income", "#svg-boston", 250000)
-	initNycMap(sf, sfdata, "Income", "#svg-sf", 250000)
+	drawFilter()
 	
-	//renderNycMap(bostondata,"Income","#svg-boston",250000)	
-	//initNycMap(nyc, nycdata, "Total", "#svg-nyc-total", 1000)	
-	//initNycMap(nyc, nycdata, "Less than 5 minutes", "#svg-nyc-0", 1000)
-	//initNycMap(nyc, nycdata, "90 or more minutes", "#svg-nyc-90", 1000)
-	drawIncome()
+	global.nyc = nyc
+	global.nycdata = nycdata
+	global.boston = boston
+	global.bostondata = bostondata
+	global.sf = sf
+	global.sfdata = sfdata
+	//call map function like this: initNycMap(path, data, column, svg, max for scale)
+	
+//	var nycdata = filterData(nycdata,0,10000000)
+	initNycMap(nyc, nycdata, "Income", "#svg-nyc", 250000)
+	
+//	var bostondata = filterData(bostondata,0,10000000)
+	initNycMap(boston, bostondata, "Income", "#svg-boston", 250000)
+	
+//	var filteredSf = filterData(sfdata,0,10000000)
+	initNycMap(sf, sfdata, "Income", "#svg-sf", 250000)
+}
+
+
+function redrawFilteredMaps(low,high){
+	//d3.select("#svg-nyc svg").remove()
+	//d3.select("#svg-boston svg").remove()
+	//d3.select("#svg-sf svg").remove()
+	
+	var nycdata = filterData(global.nycdata,low,high)
+	renderNycMap(nycdata, "Income", "#svg-nyc", 250000)
+	
+	var bostondata = filterData(global.bostondata,low,high)
+	renderNycMap(bostondata, "Income", "#svg-boston", 250000)
+	
+	var filteredSf = filterData(global.sfdata,low,high)
+//	initNycMap(global.sf, filteredSf, "Income", "#svg-sf", 250000)
+	renderNycMap(filteredSf, "Income", "#svg-sf", 250000)
+	
+	d3.select(".filterHighlight").remove()
+	
+	var y = d3.scale.linear().range([0,400]).domain([0, 250000]);
+	drawFilterHighlight(y(high),y(low))
+	console.log([high,low])
+	console.log([y(high),y(low)])
+	//var map = d3.select(#svg-sf).selectAll(".map-item")
+	d3.select("#svg-sf").selectAll(".map-item").attr("stroke-opacity", .3).attr("stroke","#999")
+	d3.select("#svg-nyc").selectAll(".map-item").attr("stroke-opacity", .3).attr("stroke","#999")
+	d3.select("#svg-boston").selectAll(".map-item").attr("stroke-opacity", .3).attr("stroke","#999")
+	
 }
 
 //put currentSelection in to global
@@ -132,27 +173,47 @@ var table = {
 	}
 }
 
-
-function drawIncome(){
-	var key = d3.select("#filters").append("svg").attr("width",200).attr("height",200)
+function drawFilter(){
+	var height = 600
+	var width = 100
+	var key = d3.select("#filters").append("svg").attr("width",width).attr("height",height)
 	var legend = key.append("defs").append("svg:linearGradient").attr("id", "gradient").attr("x1", "100%").attr("y1", "0%").attr("x2", "100%").attr("y2", "100%").attr("spreadMethod", "pad");
 	legend.append("stop").attr("offset", "0%").attr("stop-color", "#a20000").attr("stop-opacity", 1);
 	legend.append("stop").attr("offset", "100%").attr("stop-color", "#fff").attr("stop-opacity", 1);
-	key.append("rect").attr("width", 20).attr("height",200).style("fill", "url(#gradient)").attr("transform", "translate(0,10)");
-	var y = d3.scale.sqrt().range([200, 0]).domain([1, 250000]);
+	key.append("rect").attr("width", width-80).attr("height",height-200).style("fill", "url(#gradient)").attr("transform", "translate(0,0)");
+	var y = d3.scale.linear().range([height-200, 0]).domain([1, 250000]);
 	var yAxis = d3.svg.axis().scale(y).orient("right");
-	key.append("g").attr("class", "y axis").attr("transform", "translate(41,10)").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", -15).attr("dy", ".71em").style("text-anchor", "end").text("median household income");
-
-	
-	
-	
+	key.append("g").attr("class", "y axis").attr("transform", "translate(41,0)").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", -15).attr("dy", ".71em").style("text-anchor", "end").text("median household income");
+	drawFilterHighlight(height-200,0)
 }
+function drawFilterHighlight(high,low){
+	d3.select("#filters svg")
+	.append("rect")
+	.attr("class","filterHighlight")
+	.attr("width", 20)
+	.attr("height",(high)-parseInt(low))
+	.attr("y",400-high)
+	.attr("stroke","red")
+	.attr("opacity",.8)
+	.attr("stroke-width",2).attr("fill","none")
+}
+function filterData(data,low,high){
+	//console.log(data)
+	var filteredData = table.filter(table.group(data, ["Income"]), function(list, income) {
+		income = parseFloat(income)
+		return (income >= low && income <= high)
+	})
+	//console.log(filteredData)
+	return filteredData
+}
+
 
 function initNycMap(paths, data, column, svg, max) {
 	renderMap(paths, svg, global.usMapWidth,global.usMapHeight)
-	renderNycMap(data, column,svg,max)	
+	renderNycMap(data, column,svg,max)
 }
 
+//sets scale of each initial map to fit svg
 function renderMap(data, selector,width,height) {
 	var projection = d3.geo.mercator().scale(1).translate([0, 0])
 	var path = d3.geo.path().projection(projection);
@@ -185,12 +246,16 @@ function renderNycMap(data, column,svg,max) {
 
 	//console.log(companiesByZipcode)
 	var colorScale = function(d) {
-		var scale = d3.scale.sqrt().domain([1,max]).range(["#fff", "#a20000"]); 
+		var scale = d3.scale.linear().domain([1,max]).range(["#fff", "#a20000"]); 
 		var x = companiesByZipcode[d.properties.GEOID]
-		if(isNaN(x[0][column])) {
+		if(!x){
 			return scale(0)
+		}else{
+			if(isNaN(x[0][column])) {
+				return scale(0)
+			}
+			return scale(x[0][column])
 		}
-		return scale(x[0][column])
 	}
 
 	map.attr("stroke-opacity", 1)
@@ -205,17 +270,31 @@ function renderNycMap(data, column,svg,max) {
 		map.call(tip);
 		map.on('mouseover', function(d){
 			var currentZipcode = d.properties.GEOID
+			var currentIncome = table.group(data, ["Id"])[currentZipcode][0][column]
 			if(table.group(data, ["Id"])[currentZipcode]){
-				tipText = "median household income: $"+ table.group(data, ["Id"])[currentZipcode][0][column]
+				tipText = "median household income: $"+ currentIncome
 				tip.html(function(d){return tipText})
 				tip.show()
 			}else{
-				tip.html("not in data")
+				tip.html("not in income range")
 				tip.show()
 			}
+			
 		})
 		.on('mouseout', function(d){
 			tip.hide()
+		})
+		.on("click",function(d){
+			var currentZipcode = d.properties.GEOID
+			var currentIncome = table.group(data, ["Id"])[currentZipcode][0][column]			
+			var high = parseInt(currentIncome*1.1)
+			//var low = 0
+			var low = parseInt(currentIncome*0.9)
+			d3.select("#income-label").html("You selected household income of $"+currentIncome
+			+"<br/>Showing areas with income 10% above and below selection: $"+low+" - $"+high
+			+ "<br/> and additional info  - in % of areas in each city etc")
+			tip.hide()
+			redrawFilteredMaps(low,high)
 		})
 	return map
 }
